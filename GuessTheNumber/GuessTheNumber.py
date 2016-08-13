180bd449682b4ff56ded329b34971b2cf47ee283
kaleemmf <kaleemmf@US4739NB.local>
2016-08-12 20:17:08 -0400

    Initial commit

---
+++ GuessTheNumber/GuessTheNumber.py
@@ -0,0 +1,26 @@
+import random
+
+print("Hello, what is your name?")
+name = input()
+
+guesses = 0
+number = random.randint(1,20)
+print(number)
+print("Hello, %s! Do you want to play a game?"%name)
+print("Great, Im thinking of a number between 1 to 20!")
+while guesses<5:
+    print("What is your guess?")
+    answer = int(input())
+    guesses+=1
+    if answer>number:
+        print("Your guess is too high, Try again!")
+        continue
+    if answer<number:
+        print("Your guess is too low, Try again!")
+        continue
+    if answer==number:
+        break
+if answer==number:
+    print("Correct, the number was: "+str(number))
+if answer!=number:
+    print("Loser, the number was: "+str(number))
